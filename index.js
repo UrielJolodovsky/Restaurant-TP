f103f63e62efc13fcf5bfddb5f117a1ecde71dca
UrielJolodovsky <46960341@est.ort.edu.ar>
2022-09-29 15:51:40 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hola")
+
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res))
+
+app.listen(port,() => {
+    console.log()
+})
+/// ${port}
+
