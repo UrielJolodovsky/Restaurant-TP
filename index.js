66c8796274ada57e785434967a0edf9fe88f2527
UrielJolodovsky <46960341@est.ort.edu.ar>
2022-09-30 10:24:59 -0300

    Exercises 1 to 6 tried

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
-console.log("Hola")
-
 const express = require("express");
-
+const menu = require('./menu.json');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res))
+app.use(express.json());
 
-app.listen(port,() => {
-    console.log()
+// Ejercicio 1
+app.get('/menu', (req, res) => {
+    res.json(menu);
 })
-/// ${port}
+
+// Ejercicio 2
+app.get('/menu/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const plato = menu.find((plato => plato.id === id))
+    res.json(plato);    
+})
+
+// Ejercicio 3
+app.get('/principales', (req, res) => {
+    const plato_principal = menu.filter((menu) => menu.tipo === "principal");
+    res.json(plato_principal);
+})
+
+// Ejercicio 4
+app.get('/postres', (req, res) => {
+    const postres = menu.filter((menu) => menu.tipo === "postre");
+    res.json(postres);
+})
+
+// Ejercicio 5
+app.get('/bebidas', (req, res) => {
+    const bebidas = menu.filter((menu) => menu.tipo === "bebida")
+    res.json(bebidas);
+})
+
+// Ejercicio 6
+app.post('/pedido', (req, res) => {
+    const pedido = req.body.productos;
+    let precio_total = 0;
+    pedido.forEach(element => {
+        precio_total += menu.precio.filter((menu) => menu.id === id) * productos.cantidad;
+    });
+
+    res.json({"msj": "Pedido recibido", "precio":  precio_total});
+}) 
+
+// Server running
+app.listen(port, () => {
+    console.log(`Aquí está corriendo el servidor ${port}`)
+})
+
+
 
